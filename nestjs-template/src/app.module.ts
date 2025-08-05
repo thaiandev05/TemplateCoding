5c1ce597c499b1c1c3d2b89da94346e23f446002
thaiandev05 <thaiandev05@gmail.com>
2025-08-05 07:37:37 +0000

    edit

--- nestjs-template/src/app.module.ts
+++ nestjs-template/src/app.module.ts
@@ -1,36 +1,20 @@
-import { CacheModule } from '@nestjs/cache-manager';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { APP_GUARD, Reflector } from '@nestjs/core';
-import * as redisStore from 'cache-manager-ioredis';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import configuration from './common/config/configuration';
-import { AuthModule } from './modules/auth/auth.module';
-import { CustomCacheModule } from './modules/cache/cache.module';
-import { TicketModule } from './modules/ticket/ticket.module';
-import { PrismaModule } from './prisma/prisma.module';
 import { AuthCookieGuard } from './common/guard/auth.cookie.guard';
+import { PrismaModule } from './prisma/prisma.module';
 
-const CACHE_TIME_LIFE = 5 * 24 * 60 * 60
 
 @Module({
   imports: [
-    PrismaModule, AuthModule,  CustomCacheModule,
+    PrismaModule,
     ConfigModule.forRoot({
       isGlobal: true,
       load: [configuration]
-    }),
-    CacheModule.registerAsync({
-      isGlobal: true,
-      useFactory: async () => ({
-        store: redisStore,
-        host: 'localhost',
-        port: 6379,
-        ttl: CACHE_TIME_LIFE
-      }),
-    }),
-    TicketModule,
+    })
   ],
   controllers: [AppController],
   providers: [
--- nestjs-template/src/modules/ticket/ticket.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { TicketService } from './ticket.service';
-
-@Module({
-    providers: [TicketService],
-    exports: [TicketService]
-})
-export class TicketModule {}
